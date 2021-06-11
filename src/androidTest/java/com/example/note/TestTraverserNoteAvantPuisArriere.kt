ccbee9fc8698071f552bb317a10eeedff955dff5
Frédéric Gagnon <frederic.gagnon@umontreal.ca>
2021-06-11 17:11:40 -0400

    Enlever tests mal construits

--- src/androidTest/java/com/example/note/TestTraverserNoteAvantPuisArriere.kt
+++ src/androidTest/java/com/example/note/TestTraverserNoteAvantPuisArriere.kt
@@ -1,18 +1,10 @@
 package com.example.note
 
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.viewActions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import androidx.test.rule.ActivityTestRule
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 
 @RunWith(AndroidJUnit4::class)
@@ -23,7 +15,7 @@ class TestTraverserNoteAvantPuisArriere {
     val activityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun TestTraverserNoteAvantPuisArriere() {
+    fun testTraverserNoteAvantPuisArriere() {
 
     }
 }
